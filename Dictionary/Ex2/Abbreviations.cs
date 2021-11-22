766d7fd5648c6b82e3401af9992b1554b2ef4fd3
Syunich <syuunau@yahoo.co.jp>
2021-11-23 00:21:29 +0900

    2021_11_23

---
+++ Dictionary/Ex2/Abbreviations.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+
+namespace CsStudy1
+{
+    /// <summary>
+    /// 略語と対応する日本語を管理するクラス
+    /// </summary>
+    public class Abbreviations
+    {
+        private Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        public Abbreviations()
+        { 
+        }
+
+        public Abbreviations(string filepass)
+        {
+            dict = GetDictFromFile(filepass);
+        }
+
+        //要素を追加(Keyがなければうわがき)
+        public void Add(string abbr , string japanese)
+        {
+            dict[abbr] = japanese; 
+        }
+
+        public string ToAbbration(string japanese)
+        {
+            return dict.FirstOrDefault(x => x.Key == japanese).Key;
+        }
+
+        /// <summary>
+        /// 指定した日本語名が入ってるペアを返す
+        /// </summary>
+        /// <param name="japanese"></param>
+        /// <returns></returns>
+        public IEnumerable<KeyValuePair<string, string>> FindAll(string japanese)
+        {
+            foreach(var item in dict)
+            {
+                if(item.Value.Contains(japanese))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        //インデクサ(添え字でアクセス可能)
+        public string this[string abbr]
+        {
+            get { return dict.ContainsKey(abbr) ? dict[abbr] : null; }
+        }
+
+        /// <summary>
+        /// ファイルから日本語名と読みの組み合わせ Dictionaryを入手する
+        /// </summary>
+        /// <param name="filepass">テキストが書いてあるファイル名</param>
+        /// <returns></returns>
+        private Dictionary<string , string> GetDictFromFile(string filepass)
+        {
+           string[] lines = File.ReadAllLines(filepass);
+           return lines.Select(line => line.Split("=")).ToDictionary(pair => pair[0], pair => pair[1]);
+        }
+
+
+
+    }
+}
---
+++ Dictionary/Ex2/MainProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace CsStudy1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            //使う側
+            var testab = new Abbreviations("TextFile1.txt");
+            testab.Add("IEEE", "アイトリプルイー");
+            var in_O = testab.FindAll("機構");
+            foreach(var item in in_O)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+        }
+    }
+}
+
